e2becd033b8bd09b9f246dddda1317ab276bec97
SanjanaR24 <sanjana.reddy2403@gmail.com>
2023-06-14 23:02:41 -0500

    new files added

---
+++ todoApp/src/app/Header/Header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Header',
+  templateUrl: './Header.component.html',
+  styleUrls: ['./Header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  login= true;
+
+  // toggleMenu(){
+  //   if(this.login){
+  //     this.login=false;
+  //   }
+  //   else{
+  //     this.login=true;
+  //   }
+//}
+}
